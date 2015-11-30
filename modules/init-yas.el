a2fd934688f2c21dc7ad25486baf862bdb11f3dd
Gordon McShane <gordon@Gordons-MacBook-Pro.local>
2015-11-30 13:43:32 -0800

    Add a few person snippets

--- modules/init-yas.el
+++ modules/init-yas.el
@@ -1,9 +1,10 @@
 (provide 'init-yas)
 
 (use-package yasnippet
+  :diminish yas-minor-mode
   :config
   (setq yas-prompt-functions '(yas/ido-prompt
-                             yas/dropdown-prompt
-                             yas/completing-prompt
-                             yas/no-prompt))
+                               yas/dropdown-prompt
+                               yas/completing-prompt
+                               yas/no-prompt))
   (yas-global-mode +1))
