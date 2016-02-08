58ba609e6dbf4bf85d37a4c6f113ad172db246f2
Gordon McShane <gordonmcshane@gmail.com>
2016-02-08 15:13:16 -0800

    add rust-lang modes

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq load-prefer-newer t)
 
 (defvar my-init-dir (file-name-directory load-file-name)
@@ -48,6 +55,7 @@
 (require 'init-prog-mode)
 (require 'init-elisp)
 (require 'init-cxx)
+(require 'init-rust)
 (require 'init-yas)
 (require 'init-misc-modes)
 (require 'init-guide-key)
---
+++ modules/init-rust.el
@@ -0,0 +1,20 @@
+(use-package rust-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+  (use-package flycheck-rust)
+  (use-package racer
+    :init
+    (defvar racer-cmd "/Users/gordon/bin/racer/target/release/racer")
+    (defvar racer-rust-src-path "/usr/local/src/rust/src")
+    :config
+    (add-hook 'rust-mode-hook #'racer-mode)
+    (add-hook 'racer-mode-hook #'eldoc-mode)
+    (add-hook 'racer-mode-hook #'company-mode))
+  (use-package cargo
+    :config
+    (add-hook 'rust-mode-hook 'cargo-minor-mode))
+  (use-package rustfmt
+    :init
+    (defvar rustfmt-bin "/Users/gordon/.multirust/toolchains/nightly/cargo/bin/rustfmt")))
+
+(provide 'init-rust)
