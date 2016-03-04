aeeb755289713e1f0a4311bccb0be12734d44a97
Gordon McShane <gordonmcshane@gmail.com>
2016-03-04 10:45:39 -0800

    remove duplicate lines

--- modules/init-osx.el
+++ modules/init-osx.el
@@ -5,7 +5,4 @@
 ;; Enable emoji, and stop the UI from freezing when trying to display them.
 (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
 
-(set-frame-font "Source Code Pro" nil t)
-(set-face-attribute 'default nil :height 141 :weight 'light)
-
 (provide 'init-osx)
