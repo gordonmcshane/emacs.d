4657d003b648821320263a68bfa58bc7e71f3a01
Gordon McShane <gordonmcshane@gmail.com>
2016-04-14 10:21:28 -0700

    on osx, use homebrew bash

--- modules/init-osx.el
+++ modules/init-osx.el
@@ -4,4 +4,7 @@
 ;; Enable emoji, and stop the UI from freezing when trying to display them.
 (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
 
+;; use homebrew bash 4+
+(setq explicit-shell-file-name "/usr/local/bin/bash")
+
 (provide 'init-osx)
