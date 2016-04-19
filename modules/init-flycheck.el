ca098ec5cff749088923221d307f4adb92b8857d
Gordon McShane <gordonmcshane@gmail.com>
2016-04-19 00:19:31 -0700

    disable lisp checkdoc in flycheck

--- modules/init-flycheck.el
+++ modules/init-flycheck.el
@@ -1,6 +1,7 @@
 (use-package flycheck
   :config
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
   (use-package flycheck-pos-tip
     :ensure pos-tip
     :config
