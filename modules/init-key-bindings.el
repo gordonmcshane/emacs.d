d563833a63c19308c6cbe3746d54dd72d2a25b48
Gordon McShane <gordonmcshane@gmail.com>
2016-04-29 10:23:10 -0700

    Global key binding for rename-buffer-and-file

--- modules/init-key-bindings.el
+++ modules/init-key-bindings.el
@@ -12,5 +12,6 @@
 (global-set-key [f7] 'recompile-all)
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-c r") 'rename-buffer-and-file)
 
 (provide 'init-key-bindings)
