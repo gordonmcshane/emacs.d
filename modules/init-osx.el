3bfa46011b333573d2f25d7a7f4d1e655fe0dfc3
Gordon McShane <gordonmcshane@gmail.com>
2016-05-06 12:17:25 -0700

    always use font-panel with osx

--- modules/init-osx.el
+++ modules/init-osx.el
@@ -4,6 +4,8 @@
 ;; Enable emoji, and stop the UI from freezing when trying to display them.
 (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
 
+(setq mac-font-panel-mode t)
+
 ;; use homebrew bash 4+
 (setq explicit-shell-file-name "/usr/local/bin/bash")
 
