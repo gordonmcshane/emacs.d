691ea2a0eee8202c5659a1ea09f229aa98117b04
Gordon McShane <gordonmcshane@gmail.com>
2016-05-09 10:24:15 -0700

    modeline and disable modes for yas-snippet

--- modules/init-yas.el
+++ modules/init-yas.el
@@ -1,13 +1,26 @@
+(defun gdm/get-mode-hook (mode) (intern (concat (symbol-name mode) "-hook")))
+
 (use-package yasnippet
-  :diminish yas-minor-mode
-  :defer 15
+  :defer 2
   :init
+  (defvar gdm/yas-mode-line (concat " " (propertize "\uf02d" 'face '((:family "FontAwesome")))))
+
   (setq yas-prompt-functions '(yas/ido-prompt
                                yas/dropdown-prompt
                                yas/completing-prompt
                                yas/no-prompt))
 
+  (defun gdm/yas-disable-hook ()
+    (setq yas-dont-activate t))
+
+  (defvar gdm/yas-disabled-modes '(term-mode erc-mode comint-mode))
+
+  (-each
+      gdm/yas-disabled-modes
+    (lambda (mode) (add-hook (gdm/get-mode-hook mode) #'gdm/yas-disable-hook)))
+
+  :config
   (yas-global-mode +1)
-  (add-hook 'term-mode-hook (lambda () (yas-minor-mode -1))))
+  (diminish 'yas-minor-mode gdm/yas-mode-line))
 
 (provide 'init-yas)
