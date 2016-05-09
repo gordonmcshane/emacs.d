a3cc0b8bf4a42795d137a1d4ca9df35abbcc7ce0
Gordon McShane <gordonmcshane@gmail.com>
2016-05-09 10:24:42 -0700

    override mechanism for themes

--- modules/init-theme.el
+++ modules/init-theme.el
@@ -1,4 +1,12 @@
-(use-package darkokai-theme
-  :init (load-theme 'darkokai t))
+(defmacro gdm/init-theme (package-name theme-name)
+  (let ((overrides-path (make-symbol "overrides-path")))
+    `(let ((,overrides-path (expand-file-name (concat "themes/" (symbol-name ',theme-name) "-overrides.el") my-init-dir)))
+       (use-package ,package-name
+         :init
+         (load-theme ',theme-name t)
+         (when (file-exists-p ,overrides-path)
+           (load (file-name-sans-extension ,overrides-path)))))))
+
+(gdm/init-theme darkokai-theme darkokai)
 
 (provide 'init-theme)
---
+++ themes/darkokai-overrides.el
@@ -0,0 +1,9 @@
+(deftheme darkokai-overrides)
+
+(let ((class '((class color) (min-colors 257)))
+      (terminal-class '((class color) (min-colors 89))))
+
+  ;; place for personal overrides
+  ;; (custom-theme-set-variables 'darkokai-overrides)
+
+  )
