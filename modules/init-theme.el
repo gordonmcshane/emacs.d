1feba4707073176a3338b894125e4a3ef55a4e44
Gordon McShane <gordonmcshane@gmail.com>
2016-05-09 19:45:13 -0700

    use gensym / silence load in init-theme.el

--- modules/init-theme.el
+++ modules/init-theme.el
@@ -1,11 +1,13 @@
+(require 'cl)
+
 (defmacro gdm/init-theme (package-name theme-name)
-  (let ((overrides-path (make-symbol "overrides-path")))
+  (let ((overrides-path (gensym)))
     `(let ((,overrides-path (expand-file-name (concat "themes/" (symbol-name ',theme-name) "-overrides.el") my-init-dir)))
        (use-package ,package-name
          :init
          (load-theme ',theme-name t)
          (when (file-exists-p ,overrides-path)
-           (load (file-name-sans-extension ,overrides-path)))))))
+           (load (file-name-sans-extension ,overrides-path) nil t))))))
 
 (gdm/init-theme darkokai-theme darkokai)
 
